328d143d3084bcf809c43f79908d402a72911666
ytakahashi2020 <y.takahashi2020@gmail.com>
2024-12-11 09:48:41 +0900

    first commit

---
+++ createTransaction.ts
@@ -0,0 +1,35 @@
+import * as web3 from "@solana/web3.js";
+import fs from "fs";
+import path from "path";
+import { createEdgeClient } from "@Honeycomb-protocol/edge-client";
+import { sendTransactionForTests } from "@honeycomb-protocol/edge-client/client/helpers";
+
+const API_URL = "https://edge.test.honeycombprotocol.com/";
+
+const walletFile = JSON.parse(
+  fs.readFileSync(path.join("./keys/admin.json"), "utf8")
+);
+
+const keyPair = web3.Keypair.fromSecretKey(new Uint8Array(walletFile));
+
+const client = createEdgeClient(API_URL, true);
+
+const {
+  createCreateProjectTransaction: { project: projectAddress, tx: txResponse },
+} = await client.createCreateProjectTransaction({
+  name: "53w",
+  authority: keyPair.publicKey.toString(),
+  payer: keyPair.publicKey.toString(),
+});
+
+const result = await sendTransactionForTests(
+  client,
+  {
+    blockhash: txResponse.blockhash,
+    lastValidBlockHeight: txResponse.lastValidBlockHeight,
+    transaction: txResponse.transaction,
+  },
+  [keyPair]
+);
+
+console.log(result.signature);
